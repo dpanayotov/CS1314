283f3e888ff5ae38dc631d8ec19cf27f769b8d9b
dpanayotov <d.panayotov.plus@gmail.com>
2014-04-11 17:10:39 +0300

    update BitSet

--- BitSet.cpp
+++ BitSet.cpp
@@ -5,7 +5,7 @@ class BitSet
 {
     long* arr;
     int n;
-    const static int s = sizeof(long)*8;
+    const static int longBits = sizeof(long)*8;
 public:
     BitSet();
     BitSet(const BitSet&);
@@ -23,8 +23,8 @@ public:
 BitSet::BitSet()
 {
     cout<<"default"<<endl;
-    n=2;
-    arr = new long[s*n];
+    n=1;
+    arr = new long[n];
     assert(arr != NULL);
     for(int i=0; i<n; i++)
     {
@@ -36,7 +36,7 @@ BitSet::BitSet(const BitSet& other)
 {
     cout<<"copy"<<endl;
     n = other.n;
-    arr = new long[n*s*s];
+    arr = new long[n];
     assert(arr != NULL);
     for(int i=0; i<n; i++)
     {
@@ -47,8 +47,8 @@ BitSet::BitSet(const BitSet& other)
 BitSet::BitSet(int maxN)
 {
     cout<<"overload"<<endl;
-    n = s/maxN + 1;
-    arr = new long[s*maxN];
+    n = maxN/longBits + 1;
+    arr = new long[n];
     assert(arr != NULL);
     for(int i=0; i<n; i++)
     {
@@ -64,24 +64,45 @@ BitSet::~BitSet()
 
 void BitSet::add(int numBit)
 {
-    int arrNum = numBit / s; //get arr number
-    int bitNum = numBit % s;
+	//check if numBits >= amount of bits in array
+	//if so => number/(size) + 1
+	//(e.g. 250/32 +1 = 7+1=8 => we'll have 8*32=256 bits which are enough
+	if(numBit >= longBits*n)
+	{
+		int tempSize = numBit/longBits + 1;
+		long* temp = new long[tempSize];
+		assert(temp != NULL);
+		for(int i=0; i<n; i++)
+		{
+			temp[i] = arr[i];
+		}
+		delete[] arr;
+		n = tempSize;
+		arr = new long[n];
+		for(int i=0; i<n; i++)
+		{
+			arr[i] = temp[i];
+		}
+		assert(arr != NULL);
+		delete[] temp;
+	}
+    int arrNum = numBit / longBits;
+    int bitNum = numBit % longBits;
     long mask = 1 << bitNum;
     arr[arrNum] |= mask;
 }
 
 bool BitSet::member(int numBit)
 {
-    int arrNum = numBit / s;
-    int bitNum = numBit % s;
-    long mask = 1;
-    mask = 1 << bitNum;
+    int arrNum = numBit / longBits;
+    int bitNum = numBit % longBits;
+    long mask = 1 << bitNum;
     return (arr[arrNum] & mask) != 0;
 }
 
 void BitSet::print()
 {
-    for(int i=0; i<s*n; i++)
+    for(int i=0; i<longBits*n; i++)
     {
         if(member(i))
         {
@@ -92,11 +113,9 @@ void BitSet::print()
 
 int main()
 {
-    BitSet bit, bit1(2);
-    bit.add(128);
-    bit.add(30);
-    bit.add(50);
-    //cout<<bit.member(128)<<endl;
+    BitSet bit, bit1(3);
+    bit.add(365);
+    bit.add(253);
     bit.print();
     return 0;
 }
