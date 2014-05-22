4002c39c99ee06d80d169b77b22e4b4fd5158981
dpanayotov <d.panayotov.plus@gmail.com>
2014-05-22 11:23:21 +0300

    Update GameOfLife.cpp

--- GameOfLife/GameOfLife.cpp
+++ GameOfLife/GameOfLife.cpp
@@ -102,14 +102,7 @@ void GameOfLife::advance()
 	{
 		for(int j=0; j<cols; j++)
 		{
-			if(getCell(field[i][j], i, j)==ALIVE)
-			{
-				tempField[i][j] = ALIVE;
-			}
-			else
-			{
-				tempField[i][j] = DEAD;
-			}
+			tempField[i][j] = getCell(field[i][j], i, j);
 		}
 	}
 
