222fad6b941302c66f5c03a62283048cc2a2a134
dpanayotov <d.panayotov.plus@gmail.com>
2014-05-22 23:51:26 +0300

    added glider

--- GameOfLife/main.cpp
+++ GameOfLife/main.cpp
@@ -13,20 +13,39 @@ using namespace std;
 
 int main()
 {
-	int a[12] =
-	{ 0, 1, 1, 2, 2, 0, 2, 1, 2, 2 };
-	GameOfLife gol(4, 4, a, 10);
-	while (1)
-	{
-		cout << gol;
-		Sleep(300);
-		system("CLS");
-		gol.advance();
-		if (GetAsyncKeyState(VK_UP))
-		{
-			break;
-		}
-	}
+   int a[80] =
+	{       5, 1, 6, 1,
+            5, 2, 6, 2,
+			5, 11, 6, 11,
+            7, 11,4, 12,
+            3, 13, 3, 14,
+			8, 12, 9, 13, 9, 14,
+			6, 15, 6, 18,
+			4, 16, 5, 17,
+            6, 17, 7, 17,
+			3, 21, 3, 22,
+			4, 21, 4, 22,
+			5, 21, 5, 22,
+			2, 23, 2, 25,
+			1, 25, 6, 23,
+			6, 25, 7, 25,
+			3, 35, 3, 36,
+			4, 35, 4, 36
+	};
+	GameOfLife gol(11, 40, a, 70);
+	gol.advance();
+    while (1)
+    {
+        cout << gol;
+        Sleep(150);
+        system("CLS");
+        gol.advance();
+
+        if (GetAsyncKeyState(VK_UP))
+        {
+            break;
+        }
+    }
 
 	return 0;
 }
