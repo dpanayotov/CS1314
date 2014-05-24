79a82238031f68bfb3c4aa7c6296086d8fd557a4
dpanayotov <d.panayotov.plus@gmail.com>
2014-05-24 19:18:49 +0300

    Flight project

--- Java/Flight/Flight.java
+++ Java/Flight/Flight.java
@@ -4,4 +4,33 @@ package com.company;
  * Created by dpanayotov on 5/24/14.
  */
 public class Flight {
+    private DateTime arrival, departure;
+    private String from, to;
+    private int id;
+    private static int flightNumber=0;
+
+    public Flight()
+    {
+        arrival = new DateTime();
+        departure = new DateTime();
+        from = "asd";
+        to = "dsa";
+        id = ++flightNumber;
+    }
+
+    public Flight(DateTime arrival, DateTime departure, String from, String to){
+        this.arrival = arrival;
+        this.departure = departure;
+        this.from = from;
+        this.to = to;
+        id = ++flightNumber;
+    }
+
+    public final void print(){
+        System.out.printf("Flight ID: %d \nFrom: %s \nTo: %s \nDeparture: ", this.id, this.from, this.to);
+        this.departure.print();
+        System.out.print("\nArrival: ");
+        this.arrival.print();
+        System.out.println("\n");
+    }
 }
--- Java/Flight/Main.java
+++ Java/Flight/Main.java
@@ -2,9 +2,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Time t = new Time(2,3,4);
-        Date d = new Date(5,6,7);
-        DateTime da = new DateTime(d, t);
-        d.print();
+        Time time = new Time(2,3,4);
+        Date date = new Date(5,6,7);
+        DateTime dt= new DateTime(date, time);
+        Flight fl = new Flight(dt, dt, "Burgas", "Sofia");
+        Flight fl1 = new Flight(dt, dt, "Sofia", "Burgas");
+        fl.print();
+        fl1.print();
     }
 }
